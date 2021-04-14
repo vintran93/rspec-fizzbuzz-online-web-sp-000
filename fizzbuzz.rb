c6318ef215a36a13485721337268158d6d541b9b
Vincent Tran <vincentuong@yahoo.com>
2021-04-14 20:40:13 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -6,16 +6,15 @@ def fizzbuzz(int)
   if int % 3 == 0 && int % 5 == 0
     puts "FizzBuzz"
   
-
   elsif int % 3 == 0
     puts "Fizz"
-  end
+  
 
   elsif int % 5 == 0
     puts "Buzz"
-  end
+  
   
   else
     puts "nil"
-  end
+  
 end
